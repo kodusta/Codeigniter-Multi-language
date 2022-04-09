7387e69efb58868f0b78993e97b8f4dd65e7c905
Selahaddin ÇİFTÇİ <65721099+kodusta@users.noreply.github.com>
2022-04-09 23:30:37 +0300

    Update MY_Controller.php

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,17 +1,23 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
+class MY_Controller extends CI_Controller
+{
 
-	public function __construct(){
+    public function __construct()
+    {
 
 
-		parent::__construct();
-		$dil=$this->session->userdata('dil');
-		if(!$dil){
-			$dil=$this->session->set_userdata('dil','tr');
-		}
-		$this->lang->load($dil,$dil);
-	}
+        parent::__construct();
+
+        if (empty($this->session->userdata('dil'))) {
+            $this->lang->load('turkish', 'turkish');
+            $this->session->set_userdata('dil', 'turkish');
+        } else {
+            $dil = $this->session->userdata('dil');
+            $this->lang->load($dil, $dil);
+        }
+
+    }
 
 }
